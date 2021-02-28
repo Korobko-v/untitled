6322d22bc5821cdc6630fecf810aec57f0aad9ac
korobko <19kvd95@mail.ru>
2021-02-28 20:23:07 +0300

    filter

--- src/main/java/org/levelup/reflection/annotationFilter/Filter.java
+++ src/main/java/org/levelup/reflection/annotationFilter/Filter.java
@@ -10,13 +10,13 @@ import java.util.Set;
 public class Filter {
     @SneakyThrows
     public void filterClasses (String line) {
-        String pack = "org.levelup.reflection.annotationFilter." + line;
+        String pack = "org.levelup.reflection." + line;
         Reflections reflections = new Reflections(pack);
         Set<Class<?>> classes = reflections.getTypesAnnotatedWith(ReflectionClass.class);
         for (Class c : classes) {
             Class<?> newClass = Class.forName(c.getName());
             System.out.println(newClass.getDeclaredConstructor().newInstance().toString());
         }
-        }
     }
+}
 
--- src/main/java/org/levelup/reflection/annotationFilter/FilterApp.java
+++ src/main/java/org/levelup/reflection/annotationFilter/FilterApp.java
@@ -3,7 +3,6 @@ package org.levelup.reflection.annotationFilter;
 public class FilterApp {
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         Filter filter = new Filter();
-        filter.filterClasses("transport");
-        filter.filterClasses("animals");
+        filter.filterClasses("annotationFilter");
     }
 }
