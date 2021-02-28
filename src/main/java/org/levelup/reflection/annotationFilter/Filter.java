e3cd70eaf5bb4538487e6cfd9f5766b0d2eb87fa
korobko <19kvd95@mail.ru>
2021-02-28 20:05:39 +0300

    removed deprecated newInstance

--- src/main/java/org/levelup/reflection/annotationFilter/Filter.java
+++ src/main/java/org/levelup/reflection/annotationFilter/Filter.java
@@ -1,18 +1,21 @@
 package org.levelup.reflection.annotationFilter;
 
+import lombok.SneakyThrows;
 import org.levelup.reflection.ReflectionClass;
 import org.reflections.Reflections;
 
 import java.util.Set;
 
+
 public class Filter {
-    public void filterClasses (String line) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+    @SneakyThrows
+    public void filterClasses (String line) {
         String pack = "org.levelup.reflection.annotationFilter." + line;
         Reflections reflections = new Reflections(pack);
         Set<Class<?>> classes = reflections.getTypesAnnotatedWith(ReflectionClass.class);
         for (Class c : classes) {
             Class<?> newClass = c.forName(c.getName());
-            System.out.println(newClass.newInstance().toString());
+            System.out.println(newClass.getDeclaredConstructor().newInstance().toString());
         }
         }
     }
